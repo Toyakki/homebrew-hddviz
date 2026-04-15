ff427c64836c1dd3df42dca87ddcb757f6f9c105
Toyakki <totto1001@icloud.com>
2026-04-14 22:19:53 -0400

    Added version variable

--- Formula/hddviz.rb
+++ Formula/hddviz.rb
@@ -1,10 +1,13 @@
 class Hddviz < Formula
   desc "CLI tool to visualize disk usage"
   homepage "https://github.com/Toyakki/hddviz"
-  url "https://github.com/Toyakki/hddviz/releases/download/v0.1.5/hddviz_0.1.5_darwin_all.tar.gz"
-  sha256 "2afc88c360377f741766da33f55086296cec3198ed5a385ecc73425f33df42aa"
   license "GPL-3.0-only"
   version "0.1.5"
+  on_macos do
+    url "https://github.com/Toyakki/hddviz/releases/download/v#{version}/hddviz_#{version}_darwin_all.tar.gz",
+        verified: "github.com/Toyakki/hddviz/"
+    sha256 "2afc88c360377f741766da33f55086296cec3198ed5a385ecc73425f33df42aa"
+  end
 
   def install
     bin.install "hddviz"
